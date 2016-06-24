ff5e21cd0c450765cf776f8e89442d8fd51a33ec
Shivansh Rai <shivanshrai84@gmail.com>
2016-06-24 18:18:46 +0530

    Fix indentation of helper programs

--- src/ClickForFace.java
+++ src/ClickForFace.java
@@ -7,23 +7,23 @@ import java.awt.Color;
 import java.awt.event.*;
 
 public class ClickForFace extends GraphicsProgram {
-	
-	private static final double FACE_DIAM = 30;
-	
-	public void init() {
-		addMouseListeners();
-		resize(800, 600);
-	}
-	
-	
-	public void mouseClicked(MouseEvent e) {
-		GOval face = new GOval(30, 30);
-		
-		//setFilled() command is necessary to display colour
-		
-		face.setFilled(true);
-		face.setFillColor(Color.green);
-		add(face, e.getX(), e.getY());
-	}
+
+  private static final double FACE_DIAM = 30;
+
+  public void init() {
+    addMouseListeners();
+    resize(800, 600);
+  }
+
+
+  public void mouseClicked(MouseEvent e) {
+    GOval face = new GOval(30, 30);
+
+    //setFilled() command is necessary to display colour
+
+    face.setFilled(true);
+    face.setFillColor(Color.green);
+    add(face, e.getX(), e.getY());
+  }
 
 }
--- src/MouseTracker.java
+++ src/MouseTracker.java
@@ -1,5 +1,5 @@
 
-// This program displays the current location of the mouse 
+// This program displays the current location of the mouse
 
 import acm.graphics.*;
 import acm.program.*;
@@ -7,22 +7,22 @@ import acm.program.*;
 import java.awt.event.*;
 
 public class MouseTracker extends GraphicsProgram {
-	public void run() {
-		resize(800, 600);
-		
-		label = new GLabel("");
-		label.setFont("Times New Roman-36");
-		add(label, 50, 50);
-		
-		addMouseListeners();
-	}
-	
-	public void mouseMoved(MouseEvent e) {
-		label.setLabel("Mouse : (" + e.getX() + ", " + e.getY() + ")");
-	}
-	
-	
-	//the label is to be tracked between method calls, hence it is a private instance variable
-	private GLabel label;
+  public void run() {
+    resize(800, 600);
+
+    label = new GLabel("");
+    label.setFont("Times New Roman-36");
+    add(label, 50, 50);
+
+    addMouseListeners();
+  }
+
+  public void mouseMoved(MouseEvent e) {
+    label.setLabel("Mouse : (" + e.getX() + ", " + e.getY() + ")");
+  }
+
+
+  //the label is to be tracked between method calls, hence it is a private instance variable
+  private GLabel label;
 
 }
